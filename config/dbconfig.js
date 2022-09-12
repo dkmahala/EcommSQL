6f69cc1e57386be96f91cea1ffb3c6a45961292f
dkmahala <itsdkmahala@gmail.com>
2022-09-12 19:12:05 +0530

    sixth commit

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -5,21 +5,21 @@ const pool = mysql
     host: "db-mysql-blr1-36776-do-user-12395789-0.b.db.ondigitalocean.com", // HOST NAME
     user: "doadmin", // USER NAME
     database: "defaultdb", // DATABASE NAME
-    password: "AVNS_BVLVcKwefdITEHGu9xn", // DATABASE PASSWORD
+    password: "AVNS_MIELDmaRPleSxSBlVu5", // DATABASE PASSWORD
     port: 25060
   })
   .on("error", (err) => {
     console.log("Failed to connect to Database - ", err);
   });
-  console.log('connected');
 
-//   pool.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//     var sql = "CREATE TABLE Orders (order_id int not null auto_increment,p_name VARCHAR(255) not null,price int not null, product_id int not null, shipping_add VARCHAR(255) not null, primary key(order_id))";
-//     pool.query(sql, function (err, result) {
-//       if (err) throw err;
-//       console.log("Table created");
-//     });
-//   });
+
+  pool.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    // var sql = "CREATE TABLE Users (id int not null auto_increment,name VARCHAR(255) not null,email VARCHAR(255) not null, password VARCHAR(255) not null, primary key(id))";
+    // pool.query(sql, function (err, result) {
+    //   if (err) throw err;
+    //   console.log(sql);
+    // });
+   });
 module.exports = pool;
